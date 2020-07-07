53993fdf1b0960a3895148be427484a2cdfcbbec
Danielle Lucaj <danielle.lucaj@gmail.com>
2020-07-07 14:52:43 -0400

    conversions are working

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-// let cValue = document.querySelector("CInput")
-// cValue = document.getElementById("CInput").value;
+
+// let farVal = document.querySelector("#FInput");
+// let celVal = document.querySelector("#CInput");
+
+//make 4 global values for conversions to call them in the bodyLoaded fnc?
+// let cValue = document.getElementById("CInput");
+// let cTemp = (cValue * (9 / 5)) + 32;
+
+// let fValue = document.getElementById("FInput");
+// let fTemp = ((fValue - 32) * 5 / 9);
+
+
+
 function ConvertCtoF(degreesCelsius) {
     // Your code here
     let cValue = document.getElementById("CInput").value;
+    // document.getElementById("box").innerHTML = cValue
     //need to import value from text box id=CInput to be used in formula
     // let cValue = document.getElementById("CInput").nodeValue;
     degreesCelsius = cValue;
+    // console.log(parseFloat(cValue));
 
-    let temp = (cValue * (9 / 5)) + 32;
+    let cTemp = (cValue * (9 / 5)) + 32;
+    // Below allow for result to be diplayed in F Input box
+    //when line below is commented out, conversions work BUT celcius conversion isnt being printed in FInput box
+    // document.getElementById("FInput").value = temp;
 
-    // ************** MAY NOT NEED CODE BELOW *******
-    // if (temp < 32) {
-    //     document.getElementById("coldImage").style.visibility = "visible"
-    // }
-    // else if (temp >= 32 && temp <= 50) {
-    //     document.getElementById("coolImage").style.visibility = "visible"
-    // }
-    // else if (temp > 50) {
-    //     document.getElementById("warmImage").style.visibility = "visible"
-    // }
+    // document.getElementById("CInput").innerText = temp;
+
+    console.log(parseFloat(cTemp));
+    return cTemp;
 
 
-    console.log(parseFloat(temp));
-    // if (cValue = NaN) {
-    //     console.log("This is not a number");
-    // }
-    // return temp;
+
+
+
 };
 console.log(ConvertCtoF());
 
@@ -35,24 +43,27 @@ function ConvertFtoC(degreesFahrenheit) {
     let fValue = document.getElementById("FInput").value;
     //need to import info from text box id=FInput to be used in formula
     degreesFahrenheit = fValue;
+    // console.log(fValue);
 
-    let temp = ((fValue - 32) * 5 / 9);
+    let fTemp = ((fValue - 32) * 5 / 9);
+    //line of code below is good!!!
+    document.getElementById("CInput").value = fTemp;
 
     //here we connect the corresponding input to the image?
     if (fValue < 32) {
         //display cold image
         document.getElementById("coldImage").style.visibility = "visible"
-        console.log(parseFloat(temp));
+        console.log(parseFloat(fTemp));
     }
     else if (fValue >= 32 && fValue <= 50) {
         //display cool image
         document.getElementById("coolImage").style.visibility = "visible"
-        console.log(parseFloat(temp));
+        console.log(parseFloat(fTemp));
     }
     else if (fValue > 50) {
         //display warm image
         document.getElementById("warmImage").style.visibility = "visible"
-        console.log(parseFloat(temp));
+        console.log(parseFloat(fTemp));
     }
     else if (fValue = NaN) {
         //does this make a statement below convert box?
@@ -60,7 +71,7 @@ function ConvertFtoC(degreesFahrenheit) {
 
     }
 
-    return temp;
+    return fTemp;
 
 }
 console.log(ConvertFtoC());
@@ -80,7 +91,11 @@ function bodyLoaded() {
     //message for bad input?
     // document.getElementById("FInput").innerHTML = String(fValue) + " is not a number"
 
+    // document.getElementById("CInput").innerText = farVal;
+
 }
+
+//if FInput == '', then disable c to f function
 
 document.getElementById("FInput").addEventListener("click", notANumber);
 document.getElementById("CInput").addEventListener("click", notANumber);
